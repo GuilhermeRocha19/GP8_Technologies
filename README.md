0744d413aa92805d9c202e7cb4b3e9ea09054dcc
Guilherme Machado <94639316+GuilhermeRocha19@users.noreply.github.com>
2022-11-21 20:47:51 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# GP8_Technologies
+ Site Desenvolvido para Trabalho da faculdade
---
+++ script.js
@@ -0,0 +1,26 @@
+var res = document.getElementById(`res`);
+var background = document.querySelector("body#back");
+var p = document.getElementsByClassName("p");
+
+var h3 = document.getElementsByClassName("h3");
+function Aboutus() {
+  res.innerHTML = `<h2>Sobre nós!</h2> <p class="p">A GP8 Technologies foi fundada em 2022 com a proposta de oferecer soluções em digitalização de documentos, a ideia veio por conta da dificuldade que alunos passavam na hora de enviar seus documentos para suas respectivas universidades, usando fotos de má qualidade, assim perdendo horas para fazer uma simples autenticação. </p>`;
+}
+function tech() {
+  res.innerHTML = `<h2>Tecnologias!</h2> <p class="p">A GP8 Technologies garante eficiência, qualidade e agilidade de processos através da mais alta tecnologia do mercado atual. Estamos sempre nos inovando para oferecer o melhor aos nossos clientes.</p>`;
+}
+function Service() {
+  res.innerHTML = `<h2> Serviços!</h2> <p class="p">A GP8 Technologies oferece alta tecnologia e agilidade em digitalização de
+  documentos, arquivos, plantas, fotografias, documentos históricos ou antigos.
+  Trabalhando na preservação e estruturação de documentos, assim como o acesso
+  autorizado e hierarquizado aos documentos digitais ou microfilmes.</p>`;
+}
+function dell() {
+  res.innerHTML = `<h2> Dell</h2> <p class="p">Leyendo o escuchando las noticias, es evidente que la culpa es del pueblo que no sabe votar. ¿Será? Mi modesta opinión es que la culpa es de la educación, de nosotros profesores que no cumplimos con el juramento que hicimos…</p>`;
+}
+function microsoft() {
+  res.innerHTML = `<h2>Microsoft</h2> <p class="p">El último domingo estuve almorzando en la casa de una de mis hijas. Como siempre, intento evitar hablar sobre política o educación de mis nietas, pero como es habitual, no consigo resistir a una provocación. Sintetizando, durante el retorno a…</p>`;
+}
+function adp() {
+  res.innerHTML = `<h2>ADP</h2> <p class="p">O domingo estive almoçando na casa de uma de minhas filhas. Normalmente tento evitar falar sobre política ou educação das minhas netas, mas como sempre, não consigo resistir a uma provocação. Sintetizando, no retorno à minha casa, umas duas horas…</p>`;
+}
